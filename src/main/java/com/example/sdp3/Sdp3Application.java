fff52ac778cfc54b6fdc2a89d9ee3c322c054e5c
navneet7716 <navneetsingh969@gmail.com>
2021-12-04 09:07:11 +0530

    initial commit

---
+++ src/main/java/com/example/sdp3/Sdp3Application.java
@@ -0,0 +1,13 @@
+package com.example.sdp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdp3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sdp3/Sdp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sdp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
